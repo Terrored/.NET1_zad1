4b3a950e73f512eab3e38fdc96bf9d32b8fe7c8a
Daniel Wilkiewicz <danielwilkiewicz@gmail.com>
2017-11-11 16:22:15 +0100

    Add project files.

---
+++ DOTNET1_zad1/House.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET1_zad1
+{
+    class House
+    {
+       
+
+        public string Adress { get; private set; }
+        public int NumberOfMembers { get; private set; }
+
+        public int NumberOfWindows { get; private set; }
+
+        public double Area { get; private set; }
+
+        public House(string adress, int numberOfMembers, int numberOfWindows, double area)
+        {
+            Adress = adress;
+            NumberOfMembers = numberOfMembers;
+            NumberOfWindows = numberOfWindows;
+            Area = area;
+        }
+
+        public string Tax()
+        {
+            double tax;
+            tax = Area * 10;
+            return " Podatek od metrażu wynosi " + tax + " zł.";
+        }
+
+        public override string ToString()
+        {
+            return "Dom o adresie: " + Adress + ". Liczba okien: " + NumberOfWindows + ". Liczba członków rodziny: " + NumberOfMembers
+                + ". Metraż wynosi: " + Area +" m2."+ Tax();
+        }
+    }
+}
---
+++ DOTNET1_zad1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET1_zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            House house1 = new House("Do Studzienki 27/1", 3, 4, 50);
+            House house2 = new House("Miszewskiego 37/2", 7, 5, 70);
+            House house3 = new House("Wojska Polskiego 150", 1, 8, 120);
+            
+
+            List<House> ListOfHouses = new List<House>();
+            ListOfHouses.Add(house1);
+            ListOfHouses.Add(house2);
+            ListOfHouses.Add(house3);
+
+            foreach (House house in ListOfHouses)
+            {
+                Console.WriteLine("*****************************************************************");
+                Console.WriteLine(house.ToString());
+            }
+            
+
+            Console.ReadKey();
+            
+        }
+    }
+}
